281cca4a7f01245a0c2036c90f17a39adebfc260
juozaskibildis <juokibildis@gmail.com>
2017-10-08 14:10:06 +0300

    optimization

--- Restoranas/dataManagement/Database.java
+++ Restoranas/dataManagement/Database.java
@@ -2,36 +2,26 @@ package dataManagement;
 
 import java.util.Vector;
 
-public class Database 	// store everything in vectors
+public class Database
 {
 	Vector<Service> fullList = new Vector<Service>();
-	Vector<Service> serviceList = new Vector<Service>();
-	Vector<Product> productList = new Vector<Product>();
 	
-	public Service createService(String name, double price)
+	public Service create(String name, double price)
 	{
 		Service service = new Service(name, price);
 		fullList.add(service);
-		serviceList.add(service);
 		return service;
 	}
-	public Product createProduct(String name, double price, Vector<Attribute> attributes)
+	public Product create(String name, double price, Vector<Attribute> attributes)
 	{
 		Product product = new Product(name, price, attributes);
 		fullList.add(product);
-		productList.add(product);
 		return product;
 	}
 	
 	public void removeService(Service service)
 	{
-		fullList.remove(service);					// as far as I remember there is no need to check if vector contains the item before removing it
-		serviceList.remove(service);
-	}
-	public void removeProduct(Product product)
-	{
-		fullList.remove(product);
-		productList.remove(product);
+		fullList.remove(service);
 	}
 	
 }
